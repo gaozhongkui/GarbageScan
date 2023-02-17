69428797ca517a7b31e558ae0e5dd20107444300
gaozhongkui <gaozhongkui@meetstarinc.com>
2023-02-17 16:53:06 +0800

    新增：加入.c的代码进行扫描

--- app/src/main/java/com/example/garbagescan/MainActivity.kt
+++ app/src/main/java/com/example/garbagescan/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.garbagescan
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import androidx.appcompat.app.AppCompatActivity
+import com.gaozhongkui.garbagescanner.scanner.FileScanner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sdPath = Environment.getExternalStorageDirectory().absolutePath
+        val scanPath = arrayOf(sdPath)
+        val mFileScanner = FileScanner()
+        mFileScanner.setScanPath(scanPath)
+        mFileScanner.startScan(object : FileScanner.ScanCallback {
+            override fun onStart() {
+            }
+
+            override fun onFind(threadId: Long, path: String?, size: Long, modify: Long) {
+            }
+
+            override fun onFinish(isCancel: Boolean) {
+            }
+
+        })
     }
 }
