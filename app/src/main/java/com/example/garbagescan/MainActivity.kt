30d737b1d48f27346afb4f6770d75ede4ccf7d3e
gaozhongkui <gaozhongkui@meetstarinc.com>
2023-03-06 14:02:05 +0800

    修改：扫描的调用测试

--- app/src/main/java/com/example/garbagescan/MainActivity.kt
+++ app/src/main/java/com/example/garbagescan/MainActivity.kt
@@ -2,27 +2,38 @@ package com.example.garbagescan
 
 import android.os.Bundle
 import android.os.Environment
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.gaozhongkui.garbagescanner.GarbageScannerManager
+import com.gaozhongkui.garbagescanner.base.BaseScanInfo
+import com.gaozhongkui.garbagescanner.callback.IGarbageScannerCallback
+import com.gaozhongkui.garbagescanner.model.ScanItemType
 import com.gaozhongkui.garbagescanner.scanner.FileScanner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sdPath = Environment.getExternalStorageDirectory().absolutePath
-        val scanPath = arrayOf(sdPath)
-        val mFileScanner = FileScanner()
-        mFileScanner.setScanPath(scanPath)
-        mFileScanner.startScan(object : FileScanner.ScanCallback {
+        val mFileScanner = GarbageScannerManager()
+        mFileScanner.setScannerCallback(object : IGarbageScannerCallback {
             override fun onStart() {
+                Log.d(TAG, "onStart() called")
             }
 
-            override fun onFind(threadId: Long, path: String?, size: Long, modify: Long) {
+            override fun onFind(info: BaseScanInfo) {
+                Log.d(TAG, "onFind() called with: info = $info")
             }
 
-            override fun onFinish(isCancel: Boolean) {
+            override fun onFinish(mapTypes: Map<ScanItemType, List<BaseScanInfo>>) {
+                Log.d(TAG, "onFinish() called with: mapTypes = $mapTypes")
             }
 
         })
+        mFileScanner.startAllScan(this)
+
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
